299c4285c35b0dc23e3ab37a3e8fe59f70bfa406
gjergjk71 <gjergjk71@gmail.com>
2018-07-20 01:24:22 +0200

    Created custom_login view

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.views import login
 # Create your views here.
+
+def custom_login(request,**kwargs):
+	if request.user.is_authenticated:
+		return redirect("/")
+	else:
+		return login(request)
