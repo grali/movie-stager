d9e6518e4fc30e93146f064895df22c03a3aed91
gjergjk71 <gjergjk71@gmail.com>
2018-07-20 04:30:27 +0200

    Created editTopic path

--- forum/urls.py
+++ forum/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
 	path("<movie_id>",views.index,name="showForum"),
 	path("<movie_id>/create",views.createTopic,name="createTopic"),
+	path("<topic_id>/edit",views.editTopic,name="editTopic"),
 	path("<topic_id>/topic",views.showTopic,name="showTopic"),
 	path("<topic_id>/topic/createComment",views.createTopicComment,name="createComment"),
 	path("<topic_id>/topic/delete",views.deleteTopic,name="deleteTopic"),
