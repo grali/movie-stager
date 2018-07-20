7736bc783f37075e5ca84a351de964281d2a5e90
gjergjk71 <gjergjk71@gmail.com>
2018-07-20 13:02:05 +0200

    Created signUp path

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-	path("login/",views.custom_login,name="login"),
+	path("login",views.custom_login,name="login"),
+	path("signup",views.signUp,name="signUp"),
 	path("", include('django.contrib.auth.urls')),
 ]
