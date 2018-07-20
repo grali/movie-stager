4555cb5ae6a77f7e12b5ec14561da2808c3ebc1e
gjergjk71 <gjergjk71@gmail.com>
2018-07-20 04:20:06 +0200

    Created deleteTopic path

--- forum/urls.py
+++ forum/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
 	path("<movie_id>/create",views.createTopic,name="createTopic"),
 	path("<topic_id>/topic",views.showTopic,name="showTopic"),
 	path("<topic_id>/topic/createComment",views.createTopicComment,name="createComment"),
+	path("<topic_id>/topic/delete",views.deleteTopic,name="deleteTopic"),
 ]
