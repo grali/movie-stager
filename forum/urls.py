a425973fa2a8c92a46733349aa3097c07c4fa1ed
gjergjk71 <gjergjk71@gmail.com>
2018-07-20 04:10:26 +0200

    Created createComment path

--- forum/urls.py
+++ forum/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 	path("<movie_id>",views.index,name="showForum"),
 	path("<movie_id>/create",views.createTopic,name="createTopic"),
 	path("<topic_id>/topic",views.showTopic,name="showTopic"),
+	path("<topic_id>/topic/createComment",views.createTopicComment,name="createComment"),
 ]
