76b2571426fbca955a150e4165a889123d456ada
gjergjk71 <gjergjk71@gmail.com>
2018-07-20 03:03:17 +0200

    Created TopicComment model

--- forum/models.py
+++ forum/models.py
@@ -14,3 +14,14 @@ class Topic(models.Model):
 
 	def __str__(self):
 		return "{} - {} - {}".format(self.movie.name,self.user,self.title)
+
+class TopicComment(models.Model):
+	topic = models.ForeignKey(Topic,on_delete=models.CASCADE)
+	user = models.ForeignKey(User,on_delete=models.CASCADE)
+	title = models.CharField(max_length=150)
+	content = models.TextField()
+	created_at = models.DateTimeField(auto_now=True)
+	updated_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return "#{} - {} - {}".format(self.topic.id,self.user,self.title)
