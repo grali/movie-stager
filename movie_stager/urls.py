fc24f462f78628b634ad63639284c00f41ea09f6
grali <graligralih@gmail,comssh-keygen -t rsa -C YOUR@EMAIL.comgit config --global user.email graligralih@gmail,com>
2018-07-20 22:24:20 +0100

    first

--- movie_stager/urls.py
+++ movie_stager/urls.py
@@ -5,9 +5,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-	path('auth/',include("authentication.urls")),
+ path('auth/',include("authentication.urls")),
     path('movies/', include('movies.urls')),
     path('forum/',include("forum.urls")),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- movies/models.py
+++ movies/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Movie(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
+    image = models.ImageField(upload_to='movie_images')
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
