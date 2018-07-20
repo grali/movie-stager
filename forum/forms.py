df85a794be4d490769d304eaa133ceffcd0d8588
gjergjk71 <gjergjk71@gmail.com>
2018-07-20 04:10:12 +0200

    Created TopicCommentForm form

--- forum/forms.py
+++ forum/forms.py
@@ -1,8 +1,13 @@
 from django import forms
 
-from .models import Topic
+from .models import Topic,TopicComment
 
 class TopicForm(forms.ModelForm):
 	class Meta:
 		model = Topic
 		fields = ["title","content"]
+
+class TopicCommentForm(forms.ModelForm):
+	class Meta:
+		model = TopicComment
+		fields = ["content"]
