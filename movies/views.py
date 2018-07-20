d4e34678bec7ec1600523678d0bbb0365d661047
gjergjk71 <gjergjk71@gmail.com>
2018-07-20 16:10:55 +0200

    Created showMovie view

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-from .models import Movie
+from .models import Movie,Review
+from forum.models import Topic
+
 # Create your views here.
+
 def index(request):
     movies = Movie.objects.all()
     return render(request, 'movies/index.html', {
         "movies" : movies
     })
+
+def showMovie(request,movie_id):
+	movie = Movie.objects.get(pk=movie_id)
+	movie_topics = Topic.objects.filter(movie=movie)
+	movie_reviews = Review.objects.filter(movie=movie)
+	return render(request,"movies/showMovie.html",{
+		"movie":movie,
+		"movie_topics":movie_topics,
+		"movie_reviews":movie_reviews
+		})
