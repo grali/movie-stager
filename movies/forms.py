4439e011ba9944cc238122d5e379c3c7063e73c2
grali <graligralih@gmail,comssh-keygen -t rsa -C YOUR@EMAIL.comgit config --global user.email graligralih@gmail,com>
2018-07-20 14:55:15 +0100

    Movies and styling and forum

--- movies/forms.py
+++ movies/forms.py
@@ -3,14 +3,6 @@ from django import forms
 from .models import Movie, Review
 
 
-class MovieForm(forms.ModelForm):
-    class Meta:
-        model = Movie
-        fields = [
-            "name"
-            "description"
-        ]
-
 
 class ReviewForm(forms.ModelForm):
     class Meta:
