9b03ff69da2d5b0ce6204eec9e857264d9a76b33
gjergjk71 <gjergjk71@gmail.com>
2018-07-20 03:15:13 +0200

    Created index view

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from movies.models import Movie
+from .models import Topic
 # Create your views here.
+
+def index(request,movie_id):
+	movie = Movie.objects.get(pk=movie_id)
+	forum_topics = Topic.objects.filter(movie=movie)
+	context = {"movie":movie,"forum_topics":forum_topics}
+	return render(request,"forum/index.html",context)
