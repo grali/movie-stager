497c9c8e925b330ee796196a3fccd076d386ad69
gjergjk71 <gjergjk71@gmail.com>
2018-07-20 03:00:51 +0200

    Created Topic model

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from movies.models import Movie
 
 # Create your models here.
+
+class Topic(models.Model):
+	movie = models.ForeignKey(Movie,on_delete=models.CASCADE)
+	user = models.ForeignKey(User,on_delete=models.CASCADE)
+	title = models.CharField(max_length=150)
+	content = models.TextField()
+	created_at = models.DateTimeField(auto_now=True)
+	updated_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return "{} - {} - {}".format(self.movie.name,self.user,self.title)
