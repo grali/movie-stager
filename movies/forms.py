ceeabf553b8eca447f6fe73b7acfa09bec398d4a
grali <graligralih@gmail,comssh-keygen -t rsa -C YOUR@EMAIL.comgit config --global user.email graligralih@gmail,com>
2018-07-20 16:00:11 +0100

    HELPERS

--- movies/forms.py
+++ movies/forms.py
@@ -3,18 +3,6 @@ from django import forms
 from .models import Movie, Review
 
 
-<<<<<<< HEAD
-=======
-class MovieForm(forms.ModelForm):
-    class Meta:
-        model = Movie
-        fields = [
-            "name",
-            "description"
-        ]
-
->>>>>>> 589b4ade64b2070fbe8e573739e0b57fdd7f67a4
-
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
